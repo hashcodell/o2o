f1d06f8bfd564cb93ed54d32815cee91d00df68d
zlt <17316261590@163.com>
2018-12-22 17:32:13 +0800

    read/write separation

---
+++ src/main/java/com/ebaotech/o2o/dao/split/DynamicDataSourceHolder.java
@@ -0,0 +1,30 @@
+package com.ebaotech.o2o.dao.split;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DynamicDataSourceHolder {
+
+    private static Logger logger = LoggerFactory.getLogger(DynamicDataSourceHolder.class);
+    private static ThreadLocal<String> contextHolde = new ThreadLocal<String>();
+    public static final String DB_MASTER="master";
+    public static final String DB_SLAVE="slave";
+
+    public static String getDBType(){
+        String db = contextHolde.get();
+        if(db == null){
+            db = DB_MASTER;
+        }
+        return db;
+    }
+
+    public static void setDBType(String str){
+        logger.debug("所使用的数据源为：" + str);
+        contextHolde.set(str);
+    }
+
+    public static void clearDBType(){
+        contextHolde.remove();
+    }
+
+}
